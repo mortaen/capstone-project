1f64a09bf76be0eea3dd79384efed208b2d5bc31
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-12 17:43:14 +0200

    Add function for ProductForm, useState and local storage to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
-import Products from './components/ProductCardList/Products'
+import { useState } from 'react'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
+import ProductForm from './components/ProductForm/ProductForm'
+import Products from './components/Products/Products'
 
-function App({ productData }) {
+function App({ initialData }) {
+  const [productData, setProductData] = useState(() => {
+    if (localStorage.getItem('productsLocalStorage')) {
+      return JSON.parse(localStorage.getItem('productsLocalStorage'))
+    } else {
+      return initialData
+    }
+  })
+
+  function handleAddProduct({
+    productName,
+    brand,
+    imgUrl,
+    description,
+    ingredients,
+    stores,
+    energy,
+    fats,
+    carbohydrates,
+    fiber,
+    protein,
+    categories,
+    price,
+  }) {
+    const ingredientsArray = ingredients.split(',').map(item => item.trim())
+
+    const nutritionFactsObject = {
+      energy: energy,
+      fats: fats,
+      carbohydrates: carbohydrates,
+      fiber: fiber,
+      protein: protein,
+    }
+
+    const storesArray = stores.split(',').map(item => item.trim())
+
+    const categoriesArray = categories.split(',').map(item => item.trim())
+
+    const addProduct = [
+      ...productData,
+      {
+        name: productName,
+        brand: brand,
+        image: imgUrl,
+        description: description,
+        ingredients: ingredientsArray,
+        stores: storesArray,
+        nutritionFacts: nutritionFactsObject,
+        categories: categoriesArray,
+        price: price,
+      },
+    ]
+    setProductData(addProduct)
+
+    const stringifiedValue = JSON.stringify(addProduct)
+    localStorage.setItem('productsLocalStorage', stringifiedValue)
+  }
+
   return (
-    <>
-      <Products productData={productData} />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Products productData={productData} />
+        </Route>
+        <Route exact path="/product-form">
+          <ProductForm onAddProduct={handleAddProduct} />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
