2b91a2a2bf0379b4c55361e0dedb743df5078aaa
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-13 19:11:04 +0200

    Add test for Navigation

---
+++ src/components/Navigation/Navigation.spec.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter as Router } from 'react-router-dom'
+import Navigation from './Navigation'
+
+describe('NavButton', () => {
+  it('renders a button and navigation elements', () => {
+    render(
+      <Router>
+        <Navigation />
+      </Router>
+    )
+
+    const buttons = screen.getAllByRole('button')
+    expect(buttons).toHaveLength(2)
+
+    const ListItem = screen.getByText('Add Product')
+    expect(ListItem).toBeInTheDocument()
+
+    const ListItem2 = screen.getByText('Home/Products')
+    expect(ListItem2).toBeInTheDocument()
+  })
+})
