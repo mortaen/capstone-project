ce73539a8eb4330dec54edc85a900d35e123a7d5
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-13 19:09:46 +0200

    Add NavData

---
+++ src/components/Navigation/NavData.js
@@ -0,0 +1,15 @@
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineForm } from 'react-icons/ai'
+
+export const NavData = [
+  {
+    title: 'Home/Products',
+    path: '/',
+    icon: <AiOutlineHome />,
+  },
+  {
+    title: 'Add Product',
+    path: '/product-form',
+    icon: <AiOutlineForm />,
+  },
+]
