caa1b06ba53199087842989e06ecc5e58f0d1fe3
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-14 12:30:10 +0200

    Add a heading to the Products/Home page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,18 +1,24 @@
+import { AiOutlineHome } from 'react-icons/ai'
 import styled from 'styled-components/macro'
 import ProductCard from '../ProductCard/ProductCard'
 
 function Products({ productData }) {
   return (
-    <CardList>
-      {productData.map(product => (
-        <ProductCard
-          name={product.name}
-          description={product.description}
-          image={product.image}
-          key={product.name}
-        />
-      ))}
-    </CardList>
+    <>
+      <Heading>
+        <AiOutlineHome /> Products
+      </Heading>
+      <CardList>
+        {productData.map(product => (
+          <ProductCard
+            name={product.name}
+            description={product.description}
+            image={product.image}
+            key={product.name}
+          />
+        ))}
+      </CardList>
+    </>
   )
 }
 
@@ -20,6 +26,10 @@ const CardList = styled.ul`
   display: grid;
   grid-template-columns: 100%;
   padding: 0;
+`
+
+const Heading = styled.h2`
+  text-align: center;
 `
 
 export default Products
