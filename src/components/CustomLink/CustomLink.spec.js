4d8721c826afe9ba294db2e7a26d00f8449b27cb
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-14 17:12:42 +0200

    Add missing imports to CustomLink.spec.js

--- src/components/CustomLink/CustomLink.spec.js
+++ src/components/CustomLink/CustomLink.spec.js
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/dom'
 import { render } from '@testing-library/react'
+import { AiOutlineFileAdd } from 'react-icons/ai'
 import { MemoryRouter as Router } from 'react-router-dom'
+import CustomLink from './CustomLink'
 
 describe('CustomLink', () => {
   it('renders a link', () => {
