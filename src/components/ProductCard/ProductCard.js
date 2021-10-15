d92457eb00f1178b2c674a5ac209c4885532382f
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-15 17:11:17 +0200

    Add onClick redirection to ProductCard

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,8 +1,11 @@
+import { useHistory } from 'react-router'
 import styled from 'styled-components/macro'
 
-function ProductCard({ name, description, image }) {
+function ProductCard({ name, description, image, id }) {
+  const history = useHistory()
+
   return (
-    <Wrapper>
+    <Wrapper onClick={() => history.push(`/product-details/${id}`)}>
       <Image src={image} alt="" />
       <InfoWrapper>
         <h2>{name}</h2>
@@ -17,7 +20,6 @@ const Wrapper = styled.div`
   grid-template-columns: 50% 50%;
   grid-template-areas: 'image info';
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
   border-radius: 10px;
   padding: 1rem;
   margin: 10px;
