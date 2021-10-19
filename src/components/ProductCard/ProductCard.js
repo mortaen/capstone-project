cd3d65f75c74a944de761cb2adc493115a73443b
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-19 18:55:15 +0200

    Update ProductCard to incorporate Review component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,22 +1,32 @@
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import Review from '../Review/Review'
 
-function ProductCard({ name, description, image, id }) {
+function ProductCard({ name, description, image, id, onAddRating, ratings }) {
+  const history = useHistory()
+
+  function goToDetails() {
+    history.push(`/product-details/${id}`)
+  }
+
   return (
-    <StyledLink to={`/product-details/${id}`}>
+    <Wrapper onClick={goToDetails}>
       <Image src={image} alt="" />
+      <Review id={id} onAddRating={onAddRating} ratings={ratings} />
       <InfoWrapper>
         <h2>{name}</h2>
         <p>{description}</p>
       </InfoWrapper>
-    </StyledLink>
+    </Wrapper>
   )
 }
 
-const StyledLink = styled(Link)`
+const Wrapper = styled.section`
   display: grid;
   grid-template-columns: 50% 50%;
-  grid-template-areas: 'image info';
+  grid-template-areas:
+    'image info'
+    'review info';
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   padding: 1rem;
