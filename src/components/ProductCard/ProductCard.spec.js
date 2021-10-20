ab992956f5fb923e427ec2e1db5ca4da57b4c23a
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-20 10:47:05 +0200

    Modify tests to account for changes

--- src/components/ProductCard/ProductCard.spec.js
+++ src/components/ProductCard/ProductCard.spec.js
@@ -2,6 +2,13 @@ import { render, screen } from '@testing-library/react'
 import ProductCard from './ProductCard'
 import { MemoryRouter as Router } from 'react-router-dom'
 
+const exampleRatings = [
+  {
+    id: '1',
+    rating: 4,
+  },
+]
+
 describe('ProductCard', () => {
   it('renders product card elements', () => {
     render(
@@ -9,6 +16,8 @@ describe('ProductCard', () => {
         <ProductCard
           name="Oatly Haferdrink Barista Edition"
           description="Haferdrink Barista-Edition, 100% pflanzlich, perfekt zum Aufschäumen, ideal für Kaffee und Tee. Kein GMO."
+          id="1"
+          ratings={exampleRatings}
         />
       </Router>
     )
