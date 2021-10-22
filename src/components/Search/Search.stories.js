27da2ba018c8a5f485760a4b204fbabeb4e55529
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-22 12:04:02 +0200

    Add Search.stories.js

---
+++ src/components/Search/Search.stories.js
@@ -0,0 +1,15 @@
+import { action } from '@storybook/addon-actions'
+import React from 'react'
+import Search from './Search'
+
+export default {
+  title: 'Component/Search',
+  component: Search,
+}
+
+const Template = args => <Search {...args} />
+
+export const Searchbar = Template.bind({})
+Searchbar.args = {
+  setSearchQuery: action(search => console.log(search)),
+}
