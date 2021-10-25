e9f32189607e1463bf705405a4349b432a6d8321
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-25 10:30:31 +0200

    Modify useSearch so it returns seaerchResults directly instead of a function

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,59 +1,57 @@
 import { useState } from 'react'
 
-export default function useSearch() {
+export default function useSearch({ productData }) {
   const { search } = window.location
   const query = new URLSearchParams(search).get('search')
 
   const [searchQuery, setSearchQuery] = useState(query || '')
 
-  function searchProducts(productData, query) {
-    if (!query) {
-      return productData
-    } else {
-      const nameResults = productData.filter(product =>
-        product.name.toLowerCase().includes(query.toLowerCase())
-      )
-      const brandResults = productData.filter(product =>
-        product.brand.toLowerCase().includes(query.toLowerCase())
-      )
-      const descriptionResults = productData.filter(product =>
-        product.description.toLowerCase().includes(query.toLowerCase())
-      )
+  let searchResults
+  if (searchQuery !== '') {
+    const nameResults = productData.filter(product =>
+      product.name.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    const brandResults = productData.filter(product =>
+      product.brand.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    const descriptionResults = productData.filter(product =>
+      product.description.toLowerCase().includes(searchQuery.toLowerCase())
+    )
 
-      const ingredientsResults = productData.filter(product =>
-        product.ingredients
-          .map(ingredient => ingredient.toLowerCase())
-          .includes(query.toLowerCase())
-      )
+    const ingredientsResults = productData.filter(product =>
+      product.ingredients
+        .map(ingredient => ingredient.toLowerCase())
+        .includes(searchQuery.toLowerCase())
+    )
 
-      const storesResults = productData.filter(product =>
-        product.stores
-          .map(store => store.toLowerCase())
-          .includes(query.toLowerCase())
-      )
+    const storesResults = productData.filter(product =>
+      product.stores
+        .map(store => store.toLowerCase())
+        .includes(searchQuery.toLowerCase())
+    )
 
-      const categoriesResults = productData.filter(product =>
-        product.categories
-          .map(category => category.toLowerCase())
-          .includes(query.toLowerCase())
-      )
+    const categoriesResults = productData.filter(product =>
+      product.categories
+        .map(category => category.toLowerCase())
+        .includes(searchQuery.toLowerCase())
+    )
 
-      let searchResults = [].concat(
-        nameResults,
-        brandResults,
-        descriptionResults,
-        ingredientsResults,
-        storesResults,
-        categoriesResults
-      )
+    searchResults = [].concat(
+      nameResults,
+      brandResults,
+      descriptionResults,
+      ingredientsResults,
+      storesResults,
+      categoriesResults
+    )
 
-      // remove duplicates
-      searchResults = searchResults.filter((product, index) => {
-        return searchResults.indexOf(product) === index
-      })
-      return searchResults
-    }
+    // remove duplicates
+    searchResults = searchResults.filter((product, index) => {
+      return searchResults.indexOf(product) === index
+    })
+  } else {
+    searchResults = productData
   }
 
-  return { searchQuery, setSearchQuery, searchProducts }
+  return { searchQuery, setSearchQuery, searchResults }
 }
