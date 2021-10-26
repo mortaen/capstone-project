6cdad145802e323f60ef6bdc3c557d8c71692526
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-26 11:11:51 +0200

    Update data imports

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import initialData from './data.json'
+import initialProductData from './productData.json'
+import initialRecipeData from './recipeData.json'
 import GlobalStyles from './GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App initialData={initialData} />
+    <App
+      initialProductData={initialProductData}
+      initialRecipeData={initialRecipeData}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 )
