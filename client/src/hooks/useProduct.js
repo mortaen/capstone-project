afe36828b88a43fdac8812cc2cab09ba0ad3a035
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-28 17:28:02 +0200

    Update useProduct

---
+++ client/src/hooks/useProduct.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+export default function useProduct({ handleAddRating }) {
+  const [productData, setProductData] = useState([])
+
+  function handleAddProduct({
+    productName,
+    brand,
+    imgUrl,
+    description,
+    ingredients,
+    stores,
+    energy,
+    fats,
+    carbohydrates,
+    fiber,
+    protein,
+    categories,
+    price,
+    id,
+  }) {
+    const ingredientsArray = ingredients.split(',').map(item => item.trim())
+
+    const nutritionFactsObject = {
+      energy: energy,
+      fats: fats,
+      carbohydrates: carbohydrates,
+      fiber: fiber,
+      protein: protein,
+    }
+
+    const storesArray = stores.split(',').map(item => item.trim())
+
+    const categoriesArray = categories.split(',').map(item => item.trim())
+
+    const addProduct = [
+      ...productData,
+      {
+        name: productName,
+        brand: brand,
+        image: imgUrl,
+        description: description,
+        ingredients: ingredientsArray,
+        stores: storesArray,
+        nutritionFacts: nutritionFactsObject,
+        categories: categoriesArray,
+        price: price,
+        id: id,
+      },
+    ]
+    setProductData(addProduct)
+
+    const stringifiedValue = JSON.stringify(addProduct)
+    localStorage.setItem('productsLocalStorage', stringifiedValue)
+
+    handleAddRating(0, id)
+  }
+
+  return { productData, setProductData, handleAddProduct }
+}
