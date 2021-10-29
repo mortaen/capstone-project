42dec2e75cbae26a55111c0523ddf9fcb7764c98
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-29 18:03:58 +0200

    Add ClickArea to Navigation and change styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,12 +3,17 @@ import { Link } from 'react-router-dom'
 import NavButton from './NavButton'
 import styled from 'styled-components/macro'
 import { NavData } from './NavData'
+import { IconContext } from 'react-icons'
 
 function Navigation() {
   const [showNavigation, setShowNavigation] = useState(false)
 
   function toggleNavigation() {
     setShowNavigation(!showNavigation)
+  }
+
+  function closeNavigation() {
+    setShowNavigation(false)
   }
 
   return (
@@ -26,32 +31,50 @@ function Navigation() {
           return (
             <NavItem key={item.id}>
               <StyledLink to={item.path}>
-                {item.icon}
+                <IconWrapper>
+                  <IconContext.Provider
+                    value={{
+                      size: '2rem',
+                    }}
+                  >
+                    {item.icon}
+                  </IconContext.Provider>
+                </IconWrapper>
                 <Title>{item.title}</Title>
               </StyledLink>
             </NavItem>
           )
         })}
       </Nav>
+      {showNavigation && <ClickArea onClick={closeNavigation}></ClickArea>}
     </>
   )
 }
 
+const ClickArea = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 30%;
+`
+
 const Nav = styled.nav`
-  background-color: #1f2933;
+  background-color: #2f4858;
   color: white;
-  width: 50%;
+  width: 70%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   position: fixed;
   top: 0;
-  right: -50%;
+  right: -70%;
   transition: 850ms;
   list-style-type: none;
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
-  padding: 5px;
+  padding: 0.25rem;
+
   ${({ showNavigation }) =>
     showNavigation &&
     `
@@ -61,6 +84,7 @@ const Nav = styled.nav`
 
 const NavItem = styled.li`
   padding: 5px;
+  margin: 1rem 0;
 `
 
 const StyledLink = styled(Link)`
@@ -68,8 +92,15 @@ const StyledLink = styled(Link)`
   color: inherit;
 `
 
+const IconWrapper = styled.div`
+  display: inline-flex;
+  vertical-align: -8%;
+`
+
 const Title = styled.span`
   color: white;
+  font-size: 2rem;
+  padding-left: 0.5rem;
 `
 
 export default Navigation
